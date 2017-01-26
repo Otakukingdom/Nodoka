314d4dfcae5857ac3607bde934f723ff92b97061
mistlight <mistlight@otakukingdom.com>
2017-01-26 02:15:53 -0500

    cache clearing

--- src/model/ProxyManager.cpp
+++ src/model/ProxyManager.cpp
@@ -5,15 +5,23 @@
 #include "ProxyManager.h"
 #include <QDebug>
 
+static const int CACHE_SIZE_MAX = 1000;
+
 ProxyManager::ProxyManager(Core::Setting *settings) {
     this->settings = settings;
 }
 
 std::shared_ptr<AudiobookFileProxy> ProxyManager::getAudiobookFileProxy(QSqlRecord record) {
+
     auto key = record.value("full_path").toString();
     if(this->audiobookProxyCache.contains(key)) {
         return this->audiobookProxyCache.value(key);
     } else {
+        // if we have too many items in the cache, we should clear it
+        if(this->audiobookProxyCache.size() > CACHE_SIZE_MAX) {
+            this->clearCache();
+        }
+
         auto audiobookEntry = std::shared_ptr<AudiobookFileProxy>(new AudiobookFileProxy(record, this->settings));
         this->audiobookProxyCache.insert(key, audiobookEntry);
 
